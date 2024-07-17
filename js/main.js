1687137e756123eaa051333cd536e19997dbceea
Mohamed Dawood <mohamedbasyonidawood23@gmail.com>
2024-07-17 18:37:21 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let scrollTop = document.querySelector('.scroll-top');
+
+window.onscroll = function () {
+  if (scrollY >= 400) {
+    scrollTop.style.display = `flex`;
+  } else {
+    scrollTop.style.display = `none`;
+  }
+};
+
+scrollTop.onclick = function () {
+  scroll({
+    left: 0,
+    top: 0,
+    behavior: 'smooth',
+  });
+};
+
+var swiper = new Swiper('.mySwiper', {
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
