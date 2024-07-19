26bba8c938323260ba89c28c342326b4f3000af3
Mohamed Dawood <mohamedbasyonidawood23@gmail.com>
2024-07-19 15:18:14 +0300

    Add RTL Direction

--- js/main.js
+++ js/main.js
@@ -22,4 +22,50 @@ var swiper = new Swiper('.mySwiper', {
     el: '.swiper-pagination',
     clickable: true,
   },
+  rtl: document.body.getAttribute('dir') === 'rtl',
 });
+
+// change img src when change dir
+
+function changeDir() {
+  let arrowTopLeft = document.querySelector('.arrow-top');
+  let imageQuote = document.querySelectorAll('.image-quote');
+
+  const body = document.body;
+
+  const computedStyle = window.getComputedStyle(body);
+
+  const direction = computedStyle.getPropertyValue('direction');
+
+  if (direction === 'rtl') {
+    arrowTopLeft.src = 'images/Arrow-Top-Right.png';
+
+    imageQuote.forEach((image) => (image.src = 'images/Review/Left-Quote.png'));
+  } else {
+    arrowTopLeft.src = 'images/Arrow-Top-Left.png';
+    imageQuote.forEach(
+      (image) => (image.src = 'images/Review/Right-Quote.png')
+    );
+  }
+}
+function toggleDirection() {
+  const body = document.body;
+  const currentDirection = body.style.direction;
+  const rtlStylesheet = document.getElementById('rtlStylesheet');
+
+  if (currentDirection === 'rtl') {
+    body.style.direction = 'ltr';
+    rtlStylesheet.disabled = true;
+  } else {
+    body.style.direction = 'rtl';
+    rtlStylesheet.disabled = false;
+  }
+
+  changeDir();
+}
+
+document
+  .getElementById('toggleDirection')
+  .addEventListener('click', toggleDirection);
+
+changeDir();
